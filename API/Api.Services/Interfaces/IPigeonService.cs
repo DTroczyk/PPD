a41290b8362608f83951d0af26cc84a330349245
lukbar <evo3d546@gmail.com>
2020-11-24 18:05:36 +0100

    115. Tworzenie serwisu kuriera

---
+++ API/Api.Services/Interfaces/IPigeonService.cs
@@ -0,0 +1,13 @@
+﻿using Api.BLL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Services.Interfaces
+{
+    public interface IPigeonService
+    {
+        public Task<Parcel> SetStatus(string parcelId, bool parcelStatus);
+    }
+}
---
+++ API/Api.Services/Services/PigeonService.cs
@@ -0,0 +1,33 @@
+﻿using Api.BLL.Entities;
+using Api.DAL.EF;
+using Api.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Services.Services
+{
+    public class PigeonService : BaseService, IPigeonService
+    {
+        public PigeonService(ApplicationDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<Parcel> SetStatus(string parcelId, bool parcelStatus)
+        {
+            var parcelEntity = await _dbContext.Parcels
+                .Where(p => p.Id == parcelId)
+                .FirstOrDefaultAsync();
+
+            parcelEntity.IsDelivered = parcelStatus;
+
+            _dbContext.Update(parcelEntity);
+            await _dbContext.SaveChangesAsync();
+
+            return parcelEntity;
+        }
+    }
+}

fe1cef8c8f22b9d698d3dc88c955cda22073e06f
lukbar <evo3d546@gmail.com>
2020-12-08 13:33:19 +0100

    153. Utworzenie dla firm - zarys

---
+++ front/src/app/components/ForBusiness/ForBusiness.jsx
@@ -0,0 +1,36 @@
+import React from "react"
+import "./ForBusiness.css"
+
+function ForBusiness() {
+    return (
+        <div>
+            <h1>Dla firm</h1>
+            <h2>Posiadamy zachęcające oferty dla firm.</h2>
+
+            <h3>Micro Firma 150</h3>
+            <div class="paragraph">
+                <p>Abonament dla początkujących przedsiębiorców z możliwością rezygnacji bez konsekwencji do 3 miesięcy.</p>
+                <p>Dla wysyłających średnio 10 – 20 paczek miesięcznie</p>
+            </div>
+
+            <h3>Mini Firma 300</h3>
+            <div class="paragraph">
+                <p>Abonament dedykowany przedsiębiorcom, którzy stawiają pierwsze kroki w e‑commerce.</p>
+                <p>Dla wysyłających średnio 20 – 50 paczek miesięcznie</p>
+            </div>
+
+            <h3>Midi Firma 600</h3>
+            <div class="paragraph">
+                <p>Abonament dedykowany przedsiębiorcom, którzy już rozwijają swoją działalność e‑commerce.</p>
+                <p>Dla wysyłających średnio 50 – 100 paczek miesięcznie</p>
+            </div>
+
+            <h3>Potrzebujesz indywidualną ofertę abonamentu?</h3>
+            <h3>Skontaktuj się z nami.</h3>
+
+            <h3>Zachęcamy do współpracy.</h3>
+        </div>
+    )
+}
+
+export default ForBusiness

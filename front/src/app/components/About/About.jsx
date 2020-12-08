1843b1f039cdcab129f0fa57bb3c9a43f08fad2f
DTroczyk <DTroczyk@users.noreply.github.com>
2020-12-08 16:22:18 +0100

    152. Utworzenie o PPD - style

--- front/src/app/components/About/About.jsx
+++ front/src/app/components/About/About.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import './About.css'
 
 function About() {
     return (
-        <div>
-            <h1>PPD</h1>
-            <p>
+        <div className="about">
+            <h1 className="about-h1">O PPD</h1>
+            <p className="about-paragraph">
                 <strong>Pigeon Parcel Delivery</strong> to prężnie rozwijająca się firma kurierska, działająca na terenie województwa śląskiego. Dostarczamy paczki
-                do dwóch dni roboczych od czasu nadania. Pracujemy niestrudzenie niczym gołębie pocztowe, by dostarczyć Twoją paczkę najszybciej jak to możliwe.
+                do dwóch dni roboczych od czasu nadania. Pracujemy niestrudzenie niczym gołębie pocztowe, by dostarczyć Twoją paczkę najszybciej jak to możliwe. 
+            </p>
+            <p className="about-paragraph">
+
             </p>
         </div>
     )

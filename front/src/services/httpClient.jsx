c68e12826076c2f723b9e70ae2876406069d8adf
srg134 <sergiuszcode@gmail.com>
2020-12-17 16:35:07 +0100

    Poprawka adresu

--- front/src/services/httpClient.jsx
+++ front/src/services/httpClient.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const httpClient = axios.create({
-    baseURL: "http://localhost:44350/",
+    baseURL: "https://localhost:44350/",
     headers: {
         "Content-Type": "application/json",
     }

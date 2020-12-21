bbc8405c8c1b44c232f7ec962d00ffc45c1b7d16
srg134 <sergiuszcode@gmail.com>
2020-12-21 13:02:32 +0100

    Sledzenie paczki ze strony glownej

--- front/src/app/components/Home/Home.jsx
+++ front/src/app/components/Home/Home.jsx
@@ -3,10 +3,23 @@ import "./Home.css"
 
 class Home extends React.Component {
 
+    state = {
+        link: ""
+    }
+    changeValue = (v) => {
+        this.setState({link: v.target.value});
+    }
+
     render() { 
         return (
             <div id="home" className="container">
-                <div>Home</div>
+                <div className="tracking">
+                    <div class="form-group">
+                        <label for="track">Śledź przesyłkę:</label><br></br>
+                        <input type="text" class="form-control" id="track" onChange={this.changeValue} placeholder="Wpisz id paczki"/>
+                        <a href={"/tracking/"+this.state.link} class="btn btn-primary mb-2">Śledź</a>
+                    </div>
+                </div>
             </div>
         )
     }

a8f103ed33a9fc46fbec9097f05db2e1393bdb3d
Adam Wloczyk <kleevu@gmail.com>
2020-12-28 17:06:53 +0100

    Dodanie walidacji przy nadawaniu paczki - zarys

--- front/src/app/components/Send/Send.jsx
+++ front/src/app/components/Send/Send.jsx
@@ -21,7 +21,9 @@ class Send extends React.Component {
         receiverPhoneNumber: '',
         submitted: false,
         typeParcel: 'S',
-        typeParcels : []
+        typeParcels : [],
+        success: '',
+        message: ''
     }
 
     componentDidMount() {
@@ -92,7 +94,22 @@ class Send extends React.Component {
             receiverEmail,
             receiverPhoneNumber
         }
-        services.SendParcel(parcel); 
+        services.SendParcel(parcel)
+            .then(response => {
+                if(response.data==="")
+                {
+                    this.setState({success: true})
+                }
+                else
+                {
+                    this.setState({success: false})
+                    this.setState({message: response.data})
+                }      
+            })
+            .catch(error => {
+                this.setState({success: false})
+                this.setState({message: error})
+            })
 
         //Dodać komunikat, że paczka została wysłana pomyślnie. I żeby był przycisk który
         //umożliwi pobranie etykiety.
@@ -119,10 +136,20 @@ class Send extends React.Component {
                 receiverHouseNumber,
                 receiverEmail,
                 receiverPhoneNumber,
-                typeParcel
+                typeParcel,
+                success,
+                message, 
+                submitted
             } = this.state;
+
+        
+        let remail = new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+        
+        //let reCode = new RegExp(/^([0-9]{2})(-[0-9]{3})?$/);
+        //let remail = new RegExp(/^([\w.-]+)@([\w-]+)((\.(\w){2,3})+)$/);
+        //let rephone = new RegExp(/^(?<!\w)((?(+|00)?48)?)?[ -]?\d{3}[ -]?\d{3}[ -]?\d{3}(?!\w)/);
+
         return (
-            
             <div id="send" className="container">
                 <div className="row">
                     <div id="nadawca" className="col-md-6 " >
@@ -132,14 +159,20 @@ class Send extends React.Component {
                                 <div className="col-md-12">
                                     <label htmlFor="nameInputSen">Imie i nazwisko: </label>
                                     <input type="text" className="form-control" id="nameInputSen" placeholder="Wpisz imię nadawcy" name="senderName" value={senderName} onChange={this.handleChange}/>
+                                    {submitted && !senderName &&
+                                        <div className="help-block alert alert-danger">Wprowadź imie i nazwisko nadawcy!</div>
+                                    }
                                 </div>
                             </div>
                         </form>
-                        <div className="col-md-12">
+                        <div className="col-md-12 adresDiv">
                             <br></br>
                             <label htmlFor="adresInput">Adres: </label>
                             <input type="text" className="form-control" id="adresInputSen" placeholder="Podaj nazwe ulicy nadawcy" name="senderStreet" value={senderStreet} onChange={this.handleChange}/>
                             <input type="text" className="form-control" id="numerDomuInputSen" placeholder="Numer mieszkania, apartamentu, pomieszczenia, budynkui itp." name="senderHouseNumber" value={senderHouseNumber} onChange={this.handleChange}/>
+                            {submitted && !senderStreet && !senderHouseNumber &&
+                                <div className="help-block alert alert-danger">Wprowadź adres nadawcy!</div>
+                            }
                         </div>
                         <form name="form" >
                             <div className="form-row">
@@ -147,11 +180,17 @@ class Send extends React.Component {
                                     <br></br>
                                     <label htmlFor="zipInputSen">Kod pocztowy: </label>
                                     <input type="text" className="form-control" id="zipInputSen" placeholder="00-000" name="senderPostalCode" value={senderPostalCode} onChange={this.handleChange}/>
+                                    {submitted && !senderPostalCode &&
+                                        <div className="help-block alert alert-danger">Wprowadź prawdiłowy kod pocztowy nadawcy!</div>
+                                    }
                                 </div>
                                 <div className="col-md-8">
                                     <br></br>
                                     <label htmlFor="cityInputSen">Miasto: </label>
                                     <input type="text" className="form-control" id="cityInputSen" placeholder="Wpisz miasto" name="senderCity" value={senderCity} onChange={this.handleChange} />
+                                    {submitted && !senderCity &&
+                                        <div className="help-block alert alert-danger">Wprowadź miasto nadawcy!</div>
+                                    }
                                 </div>
                             </div>
                         </form>
@@ -160,7 +199,10 @@ class Send extends React.Component {
                                 <div className="col-md-6">
                                     <br></br>
                                     <label htmlFor="emailInputSen">Adres e-mail: </label>
-                                    <input type="text" className="form-control" id="emailInputSen" placeholder="mail@siec.pl" name="senderEmail" value={senderEmail} onChange={this.handleChange}/>
+                                    <input type="email" className="form-control" id="emailInputSen" placeholder="mail@siec.pl" name="senderEmail" value={senderEmail} onChange={this.handleChange}/>
+                                    {submitted && !senderEmail && !remail.test(senderEmail) &&
+                                        <div className="help-block alert alert-danger">Wprowadź prawidłowy e-mail!</div>
+                                    }
                                     <br></br>
                                     <label htmlFor="parcelSelect">Wybierz rodzaj paczki:</label>
                                     <select className="form-control" name="typeParcel" value={typeParcel} onChange={this.handleChange}>
@@ -171,6 +213,9 @@ class Send extends React.Component {
                                     <br></br>
                                     <label htmlFor="phoneInputSen">Telefon: </label>
                                     <input id="phoneInputSen" type="text" className="form-control" placeholder="123456789" name="senderPhoneNumber" value={senderPhoneNumber} onChange={this.handleChange}/>
+                                    {submitted && !senderPhoneNumber &&
+                                        <div className="help-block alert alert-danger">Wprowadź prawdidłowy numer telefonu nadawcy!</div>
+                                    }
                                 </div>
                             </div>
                         </form>
@@ -183,14 +228,20 @@ class Send extends React.Component {
                                 <div className="col-md-12">
                                     <label htmlFor="nameInputRec">Imie i nazwisko: </label>
                                     <input type="text" className="form-control" id="nameInputRec" placeholder="Wpisz imię i nazwisko odbiorcy" name="receiverName" value={receiverName} onChange={this.handleChange}/>
+                                    {submitted && !receiverName &&
+                                        <div className="help-block alert alert-danger">Wprowadź imie i nazwisko odbiorcy!</div>
+                                    }
                                 </div>
                             </div>
                         </form>
-                        <div className="col-md-12">
+                        <div className="col-md-12 adresDiv">
                             <br></br>
                             <label htmlFor="adresInput">Adres: </label>
                             <input type="text" className="form-control" id="adresInputRec" placeholder="Podaj nazwe ulicy odbiorcy" name="receiverStreet" value={receiverStreet} onChange={this.handleChange}/>
                             <input type="text" className="form-control" id="numerDomuInputRec" placeholder="Numer mieszkania, apartamentu, pomieszczenia, budynkui itp." name="receiverHouseNumber" value={receiverHouseNumber} onChange={this.handleChange}/>
+                            {submitted && !receiverStreet && !receiverHouseNumber &&
+                                <div className="help-block alert alert-danger">Wprowadź adres odbiorcy!</div>
+                            }
                         </div>
                         <form name="form" >
                             <div className="form-row">
@@ -198,11 +249,17 @@ class Send extends React.Component {
                                     <br></br>
                                     <label htmlFor="zipInputRec">Kod pocztowy: </label>
                                     <input type="text" className="form-control" id="zipInputRec" placeholder="00-000" name="receiverPostalCode" value={receiverPostalCode} onChange={this.handleChange}/>
+                                    {submitted && !receiverPostalCode &&
+                                        <div className="help-block alert alert-danger">Wprowadź prawdiłowy kod pocztowy odbiorcy!</div>
+                                    }
                                 </div>
                                 <div className="col-md-8">
                                     <br></br>
                                     <label htmlFor="cityInputRec">Miasto: </label>
                                     <input type="text" className="form-control" id="cityInputRec" placeholder="Wpisz miasto" name="receiverCity" value={receiverCity} onChange={this.handleChange}/>
+                                    {submitted && !receiverCity &&
+                                        <div className="help-block alert alert-danger">Wprowadź miasto odbiorcy!</div>
+                                    }
                                 </div>
                             </div>
                         </form>
@@ -211,12 +268,18 @@ class Send extends React.Component {
                                 <div className="col-md-6">
                                     <br></br>
                                     <label htmlFor="emailInputRec">Adres e-mail: </label>
-                                    <input type="text" className="form-control" id="emailInputRec" placeholder="mail@siec.pl" name="receiverEmail" value={receiverEmail} onChange={this.handleChange}/>
+                                    <input type="email" className="form-control" id="emailInputRec" placeholder="mail@siec.pl" name="receiverEmail" value={receiverEmail} onChange={this.handleChange}/>
+                                    {submitted && !receiverEmail && !remail.test(senderEmail) &&
+                                        <div className="help-block alert alert-danger">Wprowadź prawidłowy e-mail!</div>
+                                    }
                                 </div>
                                 <div className="col-md-6">
                                     <br></br>
                                     <label htmlFor="phoneInputRec">Telefon: </label>
                                     <input id="phoneInputRec" type="text" className="form-control" placeholder="123456789" name="receiverPhoneNumber" value={receiverPhoneNumber} onChange={this.handleChange}/>
+                                    {submitted && !receiverPhoneNumber &&
+                                        <div className="help-block alert alert-danger">Wprowadź prawdidłowy numer telefonu odbiorcy!</div>
+                                    }
                                 </div>
                             </div>
                         </form>

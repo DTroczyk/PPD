063762abd70308ccdd8302af1b5a8632b07dfc2f
Andrzej Walaszczyk <andrzej.walaszczyk@it-solve.pl>
2021-01-05 12:01:56 +0100

    Header collapse

--- front/src/app/components/Header/Header.jsx
+++ front/src/app/components/Header/Header.jsx
@@ -13,37 +13,45 @@ class Header extends React.Component {
         if (role === "Guest" || role === "Sparrow")
             return (
                 <>
-                    <li><Link to="/send">Wysyłam</Link></li>
+                    <li className="nav-item" ><Link to="/send" className="nav-link">Wysyłam</Link></li>
                     {this.props.isLoggedIn &&
-                        <li><Link to="/history">Twoja historia</Link></li>
+                        <li className="nav-item" ><Link to="/history" className="nav-link">Twoja historia</Link></li>
                     }
-                    <li><Link to="/forbusiness">Oferta dla firm</Link></li>
-                    <li><Link to="/about">O PPD</Link></li>
-                    <li><Link to="/prices">Cennik</Link></li>
-                    <li><Link to="/contact">Kontakt</Link></li>
+                    <li className="nav-item" ><Link to="/forbusiness" className="nav-link">Oferta dla firm</Link></li>
+                    <li className="nav-item" ><Link to="/about" className="nav-link">O PPD</Link></li>
+                    <li className="nav-item" ><Link to="/prices" className="nav-link">Cennik</Link></li>
+                    <li className="nav-item" ><Link to="/contact" className="nav-link">Kontakt</Link></li>
                 </>
             )
         if (role === "Stork")
             return (
-                <li><Link to="/stork">Panel Menadżera</Link></li>
+                <li className="nav-item"><Link to="/stork" className="nav-link">Panel Menadżera</Link></li>
             )
         if (role === "Pigeon")
             return (
-            <li><Link to="/pigeon">Panel Kuriera</Link></li>
+            <li className="nav-item" ><Link to="/pigeon" className="nav-link">Panel Kuriera</Link></li>
         )
     }
 
     render() { 
         return(
             <header id="header">
-                <Link to="/"><div className="img-container"><img src="/logo_500.gif" alt="PPD"></img></div></Link>
-                <ul className="header-list">
-                    {this.navigation()}
-                    {this.props.isLoggedIn
-                        ? <li onClick={this.LogOut}><Link to="/">Wyloguj się</Link></li> 
-                        : <li><Link to="/login">Zaloguj się</Link></li>
-                    }
-                </ul>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
+                    <Link to="/"><div className="navbar-brand img-container"><img src="/logo_500.gif" alt="PPD"></img></div></Link>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                        <ul className="navbar-nav">
+                            {this.navigation()}
+                            {this.props.isLoggedIn
+                                ? <li onClick={this.LogOut} class="nav-item" ><Link to="/" className="nav-link">Wyloguj się</Link></li> 
+                                : <li className="nav-item"><Link to="/login" className="nav-link">Zaloguj się</Link></li>
+                            }
+                        </ul>
+                    </div>
+                </nav>
             </header>
         );
     }
